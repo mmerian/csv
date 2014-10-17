259d730eb0827e224496c97db389642a61d3d637
Maxime Mérian <maxime.merian@gmail.com>
2014-10-17 23:17:31 +0200

    Added line count unit test

--- tests/Csv/ReaderTest.php
+++ tests/Csv/ReaderTest.php
@@ -26,4 +26,22 @@ class ReaderTest extends \PHPUnit_Framework_TestCase
         );
         $this->assertEquals($reader->getHeader(), $expectedHeader);
     }
+
+    public function testLineCount()
+    {
+        $reader = new Reader(dirname(__DIR__) . '/sample-data/us-500.csv', array(
+            'hasHeader' => true,
+            'delimiter' => ',',
+            'inputEncoding' => 'ISO-8859-15'
+        ));
+        $this->assertEquals($reader->key(), 2);
+        $i = 0;
+        foreach ($reader as $line) {
+            $i++;
+            if (5 == $i) {
+                break;
+            }
+        }
+        $this->assertEquals($reader->key(), 6);
+    }
 }
